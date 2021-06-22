b9b1b066a7e0c9df8958a02533a2e48eed6a0fbc
HotPotatoC <juandotulung@gmail.com>
2021-06-22 18:20:52 +0700

    fix: deploy failure

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
