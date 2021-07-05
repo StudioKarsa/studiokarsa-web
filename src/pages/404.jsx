ddc23d9b8faf951c6a7a432d5c93c312302b6dba
HotPotatoC <juandotulung@gmail.com>
2021-07-05 16:29:13 +0700

    fix module not found

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import Layout from '../components/Layout'
+import SEO from '../components/SEO'
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
+    <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
