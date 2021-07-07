4de9bcf7b19346b1760c0a064957e2bf06ff7026
jirenmaa <jirenmaa@gmail.com>
2021-07-07 12:05:48 +0700

    added hero and why-us section

--- src/components/landing-page/Hero.jsx
+++ src/components/landing-page/Hero.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+import { StaticImage } from "gatsby-plugin-image"
+
+import TeamworkSVG from '../../assets/images/teamwork.svg'
+
 function Hero() {
-  return <div>
-    <h1>ini hero</h1>
+  return <div className="flex flex-row items-center px-20">
+    <div className="flex-1 flex-col">
+      <div className="flex items-center uppercase font-semibold space-x-3">
+        <span>based in, indonesia</span>
+        <StaticImage
+          src="../../assets/images/indonesia-flag.png"
+          alt="indonesia flag"
+          width={20}
+          height={20}
+        />
+      </div>
+      <div className="font-bold leading-tight lg:leading-tight md:leading-normal text-4xl md:text-5xl lg:text-6xl capitalize mt-2 text-transparent bg-clip-text bg-gradient-to-br from-primary to-secondary">
+        we design &amp; build unique things
+      </div>
+      <div className="capitalize inline-block shadow-lg font-semibold bg-primary text-white p-2 px-4 mt-8 lg:p-3 lg:px-8 rounded-md">
+        contact us
+      </div>
+    </div>
+    <div className="flex-1 hidden lg:flex">
+      <TeamworkSVG className="w-full h-auto" />
+    </div>
   </div>
 }
 
--- src/components/landing-page/WhyUs.jsx
+++ src/components/landing-page/WhyUs.jsx
@@ -1,7 +1,67 @@
 import React from 'react'
 
+import CustomDesign from '../../assets/icons/custom-design.svg'
+import ModernUI from '../../assets/icons/modern-ui.svg'
+import MobileFriendly from '../../assets/icons/mobile-friendly.svg'
+
 function WhyUs() {
-  return <div>ini why us</div>
+  return <div className="flex flex-col items-center px-20 space-y-12">
+    <div className="flex flex-col text-center">
+      <div className="uppercase tracking-wide font-medium text-primary text-2xl">
+        why us?
+      </div>
+      <div className="font-semibold tracking-wider text-2xl lg:text-3xl">
+        Creating impactful digital experience
+      </div>
+    </div>
+    <div className="flex flex-col lg:flex-row justify-evenly w-full space-x-0 space-y-6 lg:space-x-12 lg:space-y-0">
+      <div className="flex flex-1 flex-col text-center space-y-4">
+        <div className="flex justify-center">
+          <div className="bg-white shadow-md rounded-full">
+            <div className="p-1">
+              <CustomDesign className="p-4 scale-75 bg-secondary bg-opacity-40 rounded-full" />
+            </div>
+          </div>
+        </div>
+        <div className="font-semibold text-xl">
+          Custom Design
+        </div>
+        <div className="text-gray-500">
+          Website kami buat dengan desain khusus dengan karakteristik dan brand bisnis berdasarkan riset mendalam
+        </div>
+      </div>
+      <div className="flex flex-1 flex-col text-center space-y-4">
+        <div className="flex justify-center">
+          <div className="bg-white shadow-md rounded-full">
+            <div className="p-1">
+              <ModernUI className="p-4 scale-75 bg-secondary bg-opacity-40 rounded-full" />
+            </div>
+          </div>
+        </div>
+        <div className="font-semibold text-xl">
+          Modern UI
+        </div>
+        <div className="text-gray-500">
+          Setiap goresan desain anda akan mengikuti trend desain antar muka paling baru
+        </div>
+      </div>
+      <div className="flex flex-1 flex-col text-center space-y-4">
+        <div className="flex justify-center">
+          <div className="bg-white shadow-md rounded-full">
+            <div className="p-1">
+              <MobileFriendly className="p-4 scale-75 bg-secondary bg-opacity-40 rounded-full" />
+            </div>
+          </div>
+        </div>
+        <div className="font-semibold text-xl">
+          Mobile Friendly
+        </div>
+        <div className="text-gray-500">
+          Website anda akan tampil dengan baik pada semua perangkat elektronik moderen
+        </div>
+      </div>
+    </div>
+  </div>
 }
 
 export default WhyUs
