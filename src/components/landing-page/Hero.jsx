1ed1d2366dd0466c8d4a371882b30413bd437a57
jirenmaa <jirenmaa@gmail.com>
2021-07-09 22:25:27 +0700

    fix resposive on smaller device

--- src/components/landing-page/Hero.jsx
+++ src/components/landing-page/Hero.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
-
 import { StaticImage } from "gatsby-plugin-image"
 
 import TeamworkSVG from '../../assets/images/teamwork.svg'
 
 function Hero() {
-  return <div className="flex flex-col flex-col-reverse lg:flex-row items-center px-6 md:px-20 lg:mt-0">
-    <div className="flex-1 flex-col">
-      <div className="flex items-center uppercase font-semibold space-x-3">
-        <span>based in jakarta, indonesia</span>
-        <StaticImage
-          src="../../assets/images/indonesia-flag.png"
-          alt="indonesia flag"
-          width={20}
-          height={20}
-        />
+  return <div className="flex flex-col flex-col-reverse items-center lg:flex-row px-6 md:px-20">
+    <div className="flex-1 flex-col space-y-2">
+      <div className="flex items-center space-x-2">
+        <div className="uppercase font-semibold md:text-lg 2xl:text-xl">
+          based in jakarta, indonesia
+        </div>
+        <div>
+          <StaticImage
+            src="../../assets/images/indonesia-flag.png"
+            alt="indonesia flag"
+            width={20}
+            height={20}
+          />
+        </div>
       </div>
-      <div className="capitalize font-bold text-transparent bg-clip-text bg-gradient-to-br from-primary to-secondary leading-tight lg:leading-normal xl:leading-tight 2xl:leading-tight text-5xl md:text-5xl lg:text-6xl 2xl:text-8xl">
+      <div className="capitalize font-bold leading-tight text-transparent bg-clip-text bg-gradient-to-br from-primary to-secondary md:leading-tight 2xl:leading-tight text-5xl md:text-6xl 2xl:text-8xl">
         we design &amp; build unique things
       </div>
-      <div className="capitalize inline-block text-white font-semibold bg-primary shadow-xl rounded-lg mt-8 text-base p-3 px-5 lg:p-3 lg:px-6 lg:text-xl 2xl:text-2xl xl:p-4 xl:px-8 2xl:p-7 2xl:px-14">
-        contact us
+      <div>
+        <button className="capitalize inline-block font-semibold text-white bg-primary shadow-xl rounded-md mt-4 py-2 px-10 text-base xl:py-3 xl:px-12 xl:py-4 xl:px-16 xl:text-xl">contact us</button>
       </div>
     </div>
-    <div className="flex-1 lg:flex">
-      <TeamworkSVG className="w-full h-auto" />
+    <div className="flex-1 flex-col">
+      <TeamworkSVG className="w-full h-auto"></TeamworkSVG>
     </div>
   </div>
 }
