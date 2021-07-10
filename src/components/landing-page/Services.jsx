7be33b2719287ba644ecf400c805ca51d56b577b
Coldersz <achmad.dzulfian7@gmail.com>
2021-07-10 22:02:15 +0700

    Update sub-title on mobile view in Services.jsx

--- src/components/landing-page/Services.jsx
+++ src/components/landing-page/Services.jsx
@@ -23,11 +23,11 @@ const Services = () => (
 const Service = (props) => {
   let icon;
   if (props.svg === 'FigmaSVG') {
-    icon = <FigmaSVG className="text-white w-16 h-16 md:w-24 md:h-24 p-2 md:p-5 flex justify-center group-hover:text-primary group-hover:transform duration-300" />
+    icon = <FigmaSVG className="text-white w-14 h-16 sm:w-16 md:w-24 md:h-24 p-2 md:p-5 flex justify-center group-hover:text-primary group-hover:transform duration-300" />
   } else if (props.svg === 'WebSVG') {
-    icon = <WebSVG className="text-white w-16 h-16 md:w-24 md:h-24 p-2 md:p-5 flex justify-center group-hover:text-primary group-hover:transform duration-300" />
+    icon = <WebSVG className="text-white w-14 h-16 sm:w-16 md:w-24 md:h-24 p-2 md:p-5 flex justify-center group-hover:text-primary group-hover:transform duration-300" />
   } else {
-    icon = <ServerSVG className="text-white w-16 h-16 md:w-24 md:h-24 p-2 md:p-5 flex justify-center group-hover:text-primary group-hover:transform duration-300" />
+    icon = <ServerSVG className="text-white w-14 h-16 sm:w-16 md:w-24 md:h-24 p-2 md:p-5 flex justify-center group-hover:text-primary group-hover:transform duration-300" />
   }
 
   return (
@@ -35,10 +35,13 @@ const Service = (props) => {
       <div className="w-full justify-center md:w-auto md:justify-start flex mb-4 md:mb-0">
         <div className="bg-primary w-full h-full md:w-24 md:h-24 group-hover:bg-white rounded-xl group-hover:transform duration-300 flex justify-center">
           {icon}
+          <h3 className="text-base sm:text-xl font-semibold text-white group-hover:text-primary group-hover:transform duration-300 self-center flex md:hidden">
+          {props.title}
+        </h3>
         </div>
       </div>
       <div className="md:place-content-around md:grid md:w-full">
-        <h3 className="text-xl md:text-2xl font-semibold group-hover:text-white group-hover:transform duration-300">
+        <h3 className="text-xl md:text-2xl font-semibold group-hover:text-white group-hover:transform duration-300 hidden md:block">
           {props.title}
         </h3>
         <p className="text-gray-500 group-hover:text-white group-hover:transform duration-300">
