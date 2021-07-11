49cd99444fdf4695f18d8f2c8577af8ad59ac792
Juan Christian <43059506+HotPotatoC@users.noreply.github.com>
2021-07-11 21:55:11 +0700

    Revert "Staging"

--- src/components/landing-page/Hero.jsx
+++ src/components/landing-page/Hero.jsx
@@ -1,36 +1,8 @@
 import React from 'react'
-import { StaticImage } from "gatsby-plugin-image"
-
-import TeamworkSVG from '../../assets/images/teamwork.svg'
 
 function Hero() {
-  return <div className="flex flex-col-reverse items-center lg:flex-row px-6 md:px-20">
-    <div className="flex-1 flex-col space-y-2">
-      <div className="flex items-center space-x-2">
-        <div className="uppercase font-semibold md:text-lg 2xl:text-xl">
-          based in jakarta, indonesia
-        </div>
-        <div>
-          <StaticImage
-            src="../../assets/images/indonesia-flag.png"
-            alt="indonesia flag"
-            width={20}
-            height={20}
-          />
-        </div>
-      </div>
-      <div className="capitalize font-bold leading-tight text-transparent bg-clip-text bg-gradient-to-br from-primary to-secondary md:leading-tight 2xl:leading-tight text-5xl md:text-6xl 2xl:text-7xl">
-        we design &amp; build unique things
-      </div>
-      <div>
-        <button className="capitalize inline-block font-semibold text-white bg-primary shadow-xl rounded-md mt-4 py-2 px-10 text-base 2xl:text-xl md:px-14 md:py-3 2xl:px-16 2xl:py-4">
-          contact us
-        </button>
-      </div>
-    </div>
-    <div className="flex-1 flex-col">
-      <TeamworkSVG className="w-full h-auto"></TeamworkSVG>
-    </div>
+  return <div>
+    <h1>ini hero</h1>
   </div>
 }
 
--- src/components/landing-page/WhyUs.jsx
+++ src/components/landing-page/WhyUs.jsx
@@ -1,67 +1,7 @@
 import React from 'react'
 
-import CustomDesign from '../../assets/icons/custom-design.svg'
-import ModernUI from '../../assets/icons/modern-ui.svg'
-import MobileFriendly from '../../assets/icons/mobile-friendly.svg'
-
 function WhyUs() {
-  return <div className="flex flex-col items-center p-12 space-y-12">
-    <div className="flex flex-col text-center">
-      <div className="text-2xl md:text-3xl text-primary tracking-widest uppercase">
-        why us?
-      </div>
-      <div className="text-2xl md:text-3xl font-semibold">
-        Creating impactful digital experience
-      </div>
-    </div>
-    <div className="flex flex-col lg:flex-row justify-evenly w-full space-x-0 space-y-6 lg:space-x-14 2xl:space-x-24 lg:space-y-0">
-      <div className="flex flex-1 flex-col text-center space-y-4">
-        <div className="flex justify-center">
-          <div className="bg-white shadow-md rounded-full p-2 2xl:p-4">
-            <div className="bg-secondary bg-opacity-40 scale-75 md:scale-90 rounded-full">
-              <CustomDesign className="scale-50 md:scale-75 rounded-full" />
-            </div>
-          </div>
-        </div>
-        <div className="font-semibold text-2xl 2xl:text-3xl">
-          Custom Design
-        </div>
-        <div className="text-gray-500 text-lg 2xl:text-2xl">
-          Website kami buat dengan desain khusus dengan karakteristik dan brand bisnis berdasarkan riset mendalam
-        </div>
-      </div>
-      <div className="flex flex-1 flex-col text-center space-y-4">
-        <div className="flex justify-center">
-          <div className="bg-white shadow-md rounded-full p-2 2xl:p-4">
-            <div className="bg-secondary bg-opacity-40 scale-75 md:scale-90 rounded-full">
-              <ModernUI className="scale-50 md:scale-75 rounded-full" />
-            </div>
-          </div>
-        </div>
-        <div className="font-semibold text-2xl 2xl:text-3xl">
-          Modern UI
-        </div>
-        <div className="text-gray-500 text-lg 2xl:text-2xl">
-          Setiap goresan desain anda akan mengikuti trend desain antar muka paling baru
-        </div>
-      </div>
-      <div className="flex flex-1 flex-col text-center space-y-4">
-        <div className="flex justify-center">
-          <div className="bg-white shadow-md rounded-full p-2 2xl:p-4">
-            <div className="bg-secondary bg-opacity-40 scale-75 md:scale-90 rounded-full">
-              <MobileFriendly className="scale-50 md:scale-75 rounded-full" />
-            </div>
-          </div>
-        </div>
-        <div className="font-semibold text-2xl 2xl:text-3xl">
-          Mobile Friendly
-        </div>
-        <div className="text-gray-500 text-lg 2xl:text-2xl">
-          Website anda akan tampil dengan baik pada semua perangkat elektronik moderen
-        </div>
-      </div>
-    </div>
-  </div>
+  return <div>ini why us</div>
 }
 
 export default WhyUs
