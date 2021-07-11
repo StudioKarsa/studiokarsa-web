35922da2843a8d212ba1809ab81d8f52a1f64724
jirenmaa <jirenmaa@gmail.com>
2021-07-11 13:41:05 +0700

    fix responsive

--- src/components/landing-page/Hero.jsx
+++ src/components/landing-page/Hero.jsx
@@ -4,7 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import TeamworkSVG from '../../assets/images/teamwork.svg'
 
 function Hero() {
-  return <div className="flex flex-col flex-col-reverse items-center lg:flex-row px-6 md:px-20">
+  return <div className="flex flex-col-reverse items-center lg:flex-row px-6 md:px-20">
     <div className="flex-1 flex-col space-y-2">
       <div className="flex items-center space-x-2">
         <div className="uppercase font-semibold md:text-lg 2xl:text-xl">
@@ -19,11 +19,13 @@ function Hero() {
           />
         </div>
       </div>
-      <div className="capitalize font-bold leading-tight text-transparent bg-clip-text bg-gradient-to-br from-primary to-secondary md:leading-tight 2xl:leading-tight text-5xl md:text-6xl 2xl:text-8xl">
+      <div className="capitalize font-bold leading-tight text-transparent bg-clip-text bg-gradient-to-br from-primary to-secondary md:leading-tight 2xl:leading-tight text-5xl md:text-6xl 2xl:text-7xl">
         we design &amp; build unique things
       </div>
       <div>
-        <button className="capitalize inline-block font-semibold text-white bg-primary shadow-xl rounded-md mt-4 py-2 px-10 text-base xl:py-3 xl:px-12 xl:py-4 xl:px-16 xl:text-xl">contact us</button>
+        <button className="capitalize inline-block font-semibold text-white bg-primary shadow-xl rounded-md mt-4 py-2 px-10 text-base 2xl:text-xl md:px-14 md:py-3 2xl:px-16 2xl:py-4">
+          contact us
+        </button>
       </div>
     </div>
     <div className="flex-1 flex-col">
