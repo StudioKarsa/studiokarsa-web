4920936bddec90dc53888dc5a3cd74cb991084e1
HotPotatoC <juandotulung@gmail.com>
2021-07-12 21:41:29 +0700

    removed unused graphql query in `Layout.jsx`

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,39 +7,24 @@
 
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
 
 import Navigation from './Navigation'
 import Footer from './Footer'
 import ScrollTop from './ScrollTop'
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(query)
-
-  return (
-    <>
-      <Navigation />
-      <main className="relative h-screen">
-        {children}
-        <Footer />
-        <ScrollTop />
-      </main>
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <>
+    <Navigation />
+    <main className="relative h-screen">
+      {children}
+      <Footer />
+      <ScrollTop />
+    </main>
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
 export default Layout
-
-const query = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
