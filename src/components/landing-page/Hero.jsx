0d1a9caec3ef319755c8a5d6ad14c7808af98950
Coldersz <achmad.dzulfian7@gmail.com>
2021-07-20 22:29:28 +0700

    Update motion transition attributes in Hero.jsx

--- src/components/landing-page/Hero.jsx
+++ src/components/landing-page/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   const { t } = useTranslation()
 
   const fadeUp = {
-    hidden: { opacity: 0, y: -20 },
+    hidden: { opacity: 0, y: 20 },
     visible: { opacity: 1, y: 0 },
   }
 
@@ -33,7 +33,7 @@ const Hero = () => {
           variants={fadeUp}
           initial="hidden"
           animate="visible"
-          transition={{ duration: 0.7, delay: 0.3 }}
+          transition={{ duration: 3.2, delay: 0.4, ease: [0.19, 1.0, 0.22, 1.0] }}
           className="flex items-center space-x-2"
         >
           <div className="uppercase font-semibold md:text-lg 2xl:text-xl">
@@ -52,16 +52,16 @@ const Hero = () => {
           variants={fadeUp}
           initial="hidden"
           animate="visible"
-          transition={{ duration: 0.7, delay: 0.6 }}
+          transition={{ duration: 3.2, delay: 0.7, ease: [0.19, 1.0, 0.22, 1.0] }}
           className="capitalize font-bold leading-tight text-transparent bg-clip-text bg-gradient-to-br from-primary to-secondary md:leading-tight 2xl:leading-tight text-5xl md:text-6xl"
         >
           {t('landingPage.hero.headline')}
         </motion.div>
         <motion.div
-          variants={fadeLeft}
+          variants={fadeUp}
           initial="hidden"
           animate="visible"
-          transition={{ duration: 0.7, delay: 0.9 }}
+          transition={{ duration: 3.2, delay: 1, ease: [0.19, 1.0, 0.22, 1.0] }}
         >
           <button className="capitalize inline-block font-semibold text-white bg-primary shadow-xl rounded-md mt-4 py-2 px-10 text-base 2xl:text-xl md:px-14 md:py-3 2xl:px-16 2xl:py-4 hover:transform hover:-translate-y-1 duration-300">
             contact us
@@ -72,7 +72,7 @@ const Hero = () => {
         variants={fadeRight}
         initial="hidden"
         animate="visible"
-        transition={{ duration: 0.7 }}
+        transition={{ duration: 3.2, ease: [0.19, 1.0, 0.22, 1.0] }}
         className="flex-1 flex-col"
       >
         <TeamworkSVG className="w-full h-auto"></TeamworkSVG>
