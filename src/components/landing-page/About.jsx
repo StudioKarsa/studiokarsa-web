fd63ee686aea3a77b91e9cedd2c6d7f7584e1f12
Andrien Wiandyano <arnnied03@gmail.com>
2021-07-21 14:10:36 +0700

    modify 'About.jsx' to use i18n

--- src/components/landing-page/About.jsx
+++ src/components/landing-page/About.jsx
@@ -1,42 +1,43 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Link } from 'gatsby'
 
 import TeamSVG from '../../assets/images/team.svg'
 import ArrowSVG from '../../assets/icons/arrow.svg'
 
-const About = () => (
-  <div id="section-about" className="flex flex-col px-6 md:px-20 space-y-2">
-    <div className="flex flex-col">
-      <h2 className="text-2xl lg:text-3xl font-medium text-primary tracking-wide uppercase">
-        about
-      </h2>
-      <h2 className="text-2xl lg:text-4xl font-semibold tracking-wider">
-        Who are we?
-      </h2>
-    </div>
-    <div className="flex flex-col md:flex-row justify-between">
-      <div className="w-full md:w-1/2 p-2 md:p-8">
-        <TeamSVG className="w-full h-full" />
+const About = () => {
+  const { t } = useTranslation()
+
+  return (
+    <div id="section-about" className="flex flex-col px-6 md:px-20 space-y-2">
+      <div className="flex flex-col">
+        <h2 className="text-2xl lg:text-3xl font-medium text-primary tracking-wide uppercase">
+          {t('landingPage.about.title')}
+        </h2>
+        <h2 className="text-2xl lg:text-4xl font-semibold tracking-wider">
+          {t('landingPage.about.subtitle')}
+        </h2>
       </div>
-      <div className="flex w-full md:w-1/2 p-2 md:p-8 text-lg">
-        <div className="my-auto">
-          <p>
-            We are a remote digital agency created by some college students
-            based in Jakarta, Indonesia. which consist of 8 people who each have
-            various experiences in IT, Software Development and UI / UX Design.
-          </p>
-          <br />
-          <Link
-            to="/"
-            className="group font-semibold flex flex-row items-center hover:underline"
-          >
-            Learn More
-            <ArrowSVG className="w-5 h-5 ml-12 group-hover:transform group-hover:translate-x-4 duration-200" />
-          </Link>
+      <div className="flex flex-col md:flex-row justify-between">
+        <div className="w-full md:w-1/2 p-2 md:p-8">
+          <TeamSVG className="w-full h-full" />
+        </div>
+        <div className="flex w-full md:w-1/2 p-2 md:p-8 text-lg">
+          <div className="my-auto">
+            <p>{t('landingPage.about.content')}</p>
+            <br />
+            <Link
+              to="/"
+              className="group font-semibold flex flex-row items-center hover:underline"
+            >
+              Learn More
+              <ArrowSVG className="w-5 h-5 ml-12 group-hover:transform group-hover:translate-x-4 duration-200" />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default About
